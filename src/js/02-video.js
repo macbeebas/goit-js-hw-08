df3f39ccc35db227c603645eaa3a62810d9d69ed
Michał Viwegier <beebas@wp.pl>
2023-08-20 18:05:32 +0200

    1 & 2 done, 3 without 'throttle' yet

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+if (localStorage.getItem('videoplayer-current-time') != null) {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(currentTime);
+}
+
+player.on(
+  'timeupdate',
+  throttle(timer => {
+    localStorage.setItem('videoplayer-current-time', timer.seconds);
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+let throttle = require('lodash.throttle');
 
+const inputValue = {};
+const emailInput = document.querySelector("input[name='email']");
+const messageInput = document.querySelector("textarea[name='message']");
+const formSubmit = document.querySelector('.feedback-form');
+
+if (localStorage.getItem('feedback-form-state') == null) {
+  inputValue.email = '';
+  inputValue.message = '';
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputValue));
+} else {
+  const currentValue = JSON.parse(localStorage.getItem('feedback-form-state'));
+  inputValue.email = currentValue.email;
+  emailInput.value = currentValue.email;
+  inputValue.message = currentValue.message;
+  messageInput.value = currentValue.message;
+}
+
+emailInput.addEventListener('input', event => {
+  inputValue.email = event.currentTarget.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputValue));
+});
+
+messageInput.addEventListener('input', event => {
+  inputValue.message = event.currentTarget.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputValue));
+});
+
+formSubmit.addEventListener('submit', event => {
+  event.preventDefault();
+  const localStorageValue = localStorage.getItem('feedback-form-state');
+  console.log(localStorageValue);
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+});
